6108bae0249191f874e86d3aef6cbc2adbe8cef2
Joe Hinkle <joehinkle11@gmail.com>
2022-05-29 03:40:21 -0500

    Upgraded to SwiftGit3 1.2.2 where all binary deps are XCFrameworks. And exposing targets to product library

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GitProviders",
-            targets: ["GitProviders"]),
+            targets: ["GitProviders", "GitClient", "GitAPI"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
-        .package(name: "SwiftGit2", url: "https://github.com/App-Maker-Software/SwiftGit3.git", from: "1.2.0"),
+        .package(name: "SwiftGit2", url: "https://github.com/App-Maker-Software/SwiftGit3.git", from: "1.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
